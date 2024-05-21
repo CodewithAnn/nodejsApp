e6fa0dad3da6b7a3180b08db1b202575be5370d2
Anand jha <codewithann@gmail.com>
2024-05-22 00:15:16 +0530

    setup express and mongodb
        - use connection to conect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //console.log("Hello world");
 
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 app.get('/', (req,res) => {
-    res.send('Hello World of Node Api');
+    res.send('Hello World of Node Api' + 'With Anand Jha');
 });
+
+mongoose.connect("mongodb+srv://anandjha:anand123@cluster0.kvbey4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+    console.log('connection successful');
+}).catch(() => {
+    console.log("connection failed");
+}); 
