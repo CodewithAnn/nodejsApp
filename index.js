8729001d4efd7fe239955b6db79b1254bbd68c45
Anand jha <codewithann@gmail.com>
2024-05-21 19:11:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//console.log("Hello world");
+
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
+app.get('/', (req,res) => {
+    res.send('Hello World of Node Api');
+});
